012db3977ca30c9f5f085fef29a5724ca5451958
QIT <2278949918@qq.com>
2023-11-05 19:28:58 +0800

    new

--- 练习/严书练习/数组和广义表.cpp
+++ 练习/严书练习/数组和广义表.cpp
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include "Status.h"
 #include <limits.h>
-
+//TODO:串的定长顺序存储表示
+#define MAXSIZE 255
+typedef unsigned char SString[MAXSIZE+1];//0号单元存放串的长度
 //TODO:数组的顺序存储表示和实现
 //-------------------------数组的顺序存储表示---------------------------
 #include <stdarg.h>
@@ -58,10 +60,10 @@ Status Locate(Array A,va_list ap,int &off){
     return OK;
 }
 //读取元素
-Status Value(Array A,ElemType &e,...){
+Status Value(Array A,ElemType *e,...){
     va_list ap;va_start(ap,e);int off;
     if(Locate(A,ap,off)<=0) return ERROR;
-    e=*(A.base+off);
+    *e=*(A.base+off);
     return OK; 
 }
 //修改元素
@@ -73,7 +75,6 @@ Status Assign(Array &A,ElemType e,...){
 }
 //TODO:三元组顺序表
 //-----------------------稀疏矩阵的三元组顺序表存储表示--------------------
-# define MAXSIZE 12500
 typedef struct{
     int i,j;
     ElemType e;
@@ -181,15 +182,17 @@ struct CrossList{
 
 //Create
 Status CreateSMatrix_OL(CrossList &M){
-    if(M.rhead) delete []M.rhead;
-    if(M.chead) delete []M.chead;
-    scanf("行数:%d,列数:%d,非零元个数:%d",&M.mu,&M.nu,&M.tu);
+    if(M.rhead) free(M.rhead);
+    if(M.chead) free(M.chead);
+    printf("输入矩阵的行号列号非零元个数\n");
+    scanf("%d,%d,%d",&M.mu,&M.nu,&M.tu);
     M.rhead=(OLink*)calloc(M.mu+1,sizeof(OLink));
     if(!M.rhead)    exit(OVERFLOW);
     M.chead=(OLink*)calloc(M.nu+1,sizeof(OLink));
     if(!M.chead)    exit(OVERFLOW);
     int i,j,e,count=0;
-    for(scanf("行号:%d,列号:%d,数据:%d",&i,&j,&e);i>0&&j>0&&count<M.tu;scanf("行号:%d,列号:%d,数据:%d",&i,&j,&e)){
+    printf("非零元信息\n");
+    for(scanf("%d,%d,%d",&i,&j,&e);i>0&&j>0&&count<M.tu;){
         OLink p=(OLink)malloc(sizeof(OLNode));
         if(!p)  exit(OVERFLOW);
         p->i=i;p->j=j;p->e=e;
@@ -198,18 +201,272 @@ Status CreateSMatrix_OL(CrossList &M){
             p->right=M.rhead[i];M.rhead[i]=p;
         }else{
             OLink q=M.rhead[i];
-            while(!q->right&&q->right->j<j)   q=q->right;
-            p->right=q->right;q->right=q;
+            while(q->right&&q->right->j<j)   q=q->right;
+            p->right=q->right;q->right=p;
         }
         //列插入
-        if(!M.chead[i]||M.chead[i]->j>j){
-            p->down=M.chead[i];M.chead[i]=p;
+        if(!M.chead[j]||M.chead[j]->i>i){
+            p->down=M.chead[j];M.chead[j]=p;
         }else{
-            OLink q=M.chead[i];
-            while(!q->down&&q->down->j<j)   q=q->down;
-            p->down=q->down;q->down=q;
+            OLink q=M.chead[j];
+            while(q->down&&q->down->j<j)   q=q->down;
+            p->down=q->down;q->down=p;
         }
         count++;
+        if(count<M.tu)  scanf("%d,%d,%d",&i,&j,&e);
     }
     return OK;
 }
+
+//Addition
+/*
+逐行进行加法
+    1.在进行节点操作时，因为会有删除和插入操作，所以需要记录两个前驱结点
+    2.因为每次循环中行数是固定的，所以行前驱结点pre只需设立一个;而列数不确定，所以需要建立一个指针数组hl来记录每列的前驱结点
+    3.pre指向的是pa的行前驱结点,而hl用来指向列中处理完的最后一个结点;两者处理不同的原因是无法确定下一个p的列数，但可以确定行数。
+        因此当插入行的第一个结点时，无法确定下一个p的列数大小，即不能更新pre，只有确定了pa->j<p->j时，才可更新pre;
+*/
+Status AdditionMatrix(CrossList &A,CrossList B){
+    OLNode*pa=A.rhead[1],*pb=B.rhead[1],*pre=nullptr;   //pre指向pa的前驱结点
+    OLink*hl=(OLink*)calloc(A.nu+1,sizeof(OLink));
+    if(!hl) exit(OVERFLOW);
+    for(int i=1;i<=A.nu;i++)    hl[i]=A.chead[i];
+    for(int row=1;row<=A.mu;row++,pa=A.rhead[row],pb=B.rhead[row]){
+        while(pb){
+            //复制pb结点
+            if(pa==nullptr||pa->j>pb->j){
+                OLNode*p=(OLNode*)malloc(sizeof(OLNode));
+                if(!p)  exit(OVERFLOW);
+                *p=*pb;
+                //处理行
+                if(!pa){
+                    A.rhead[row]=p;
+                }else if(pre==nullptr){
+                    A.rhead[row]=p;p->right=pa;
+                }else{
+                    pre->right=p;
+                }p->right=pa;pre=p;
+                //处理列
+                if(!A.chead[p->j]||A.chead[p->j]->i>p->i){
+                    p->down=A.chead[p->j];A.chead[p->j]=p;hl[p->j]=p;
+                }else{
+                    p->down=hl[p->j]->down;hl[p->j]->down=p;
+                }hl[p->j]=p;
+            }else if(pa->j<pb->j){
+                pre=pa;pa=pa->right;
+            }else if(pa->j==pa->j){
+                pa->e+=pb->e;
+                if(pa->e==0){
+                    if(pre==nullptr){
+                        A.rhead[row]=pa->right;
+                    }else{
+                        pre->right=pa->right;
+                    }
+                    if(A.chead[pa->j]==pa){
+                        A.chead[pa->j]=pa->down;
+                    }else{
+                        hl[pa->j]->down=pa->down;
+                    }free(pa);
+
+                }
+                
+            }
+            pb=pb->right;
+        }
+    }
+    return OK;
+}
+void PrintMatrix(CrossList M) {
+    for (int i = 1; i <= M.mu; i++) {
+        OLink p = M.rhead[i];
+        for (int j = 1; j <= M.nu; j++) {
+            if (p && p->j == j) {
+                // 使用 printf 输出稀疏矩阵元素
+                printf("%d ", p->e);
+                p = p->right;
+            } else {
+                printf("0 ");
+            }
+        }
+        printf("\n");
+    }
+}
+//TODO:广义表的存储结构
+//----------------------------广义表的头尾链表存储结构--------------------------------------
+typedef enum{ATOM,LIST} ElemTag;    //ATOM==0:原子,LIST==2:子表
+#define AtomType int
+typedef struct GHTLNode{
+    ElemTag tag;
+    union{
+        AtomType atom;
+        struct{GHTLNode*hp,*tp;}ptr;
+    };
+}*GHTList;
+//---------------------------广义表的扩展线性链表存储表示------------------------------------
+typedef struct GLLNode{
+    ElemTag tag;
+    union{
+        AtomType atom;
+        struct GLLNode *hp;
+    };
+    GLLNode *tp;    //相当于next
+}*GLList;
+//--------------------------m元多项式的广义表的存储结构------------------------------
+typedef struct MPNode{
+    ElemTag tag;
+    int exp;
+    union{
+        float   coef;   //系数域
+        MPNode *hp;
+    };
+    MPNode *tp;
+}*MPList;
+//TODO:广义表的递归算法
+//---------------------------------求广义表的深度--------------------------------------
+/*
+基本项：DEPTH(LS)=1  LS为空表
+       DEPTH(LS)=0  LS为原子
+归纳项：DEPTH(LS)=1+MAX{DEPTH(a<i>)} 
+*/
+int GHTListDepth(GHTList L){
+    if(!L)  return 1;
+    if(L->tag==0)   return 0;
+    GHTList p=nullptr;int max;
+    for(max=0,p=L;p;p=p->ptr.tp){
+        int d=GHTListDepth(p->ptr.hp);
+        if(max<d)   max=d;
+    }
+    return max+1;
+}
+//--------------------------------复制广义表------------------------------------------------
+/*
+基本项:置空表,当LS为空表;
+归纳项:1.复制表头;2.复制表尾
+*/
+Status CopyGHTList(GHTList &T,GHTList L){
+    if(!L)  T=nullptr;
+    else{
+        T=(GHTList)malloc(sizeof(GHTLNode));
+        if(!T)  exit(OVERFLOW);
+        T->tag=L->tag;
+        if(T->tag==0){
+            T->atom=L->atom;
+        }else{
+            CopyGHTList(T->ptr.hp,L->ptr.hp);
+            CopyGHTList(T->ptr.tp,L->ptr.tp);
+        }
+    }
+    return OK;
+}
+//-----------------------------------建立广义表--------------------------------
+/*
+基本项：置空广义表  当S为空表串时
+        建原子节点的子表    当S为单字符串时
+归纳项：假设sub为脱去S最外层括弧的子串，记为's1,s2,...,sn'，其中si的为非空字符串。对每一个si建立一个表结点，
+        并令其hp域的指针为由si建立的子表的头指针，除最后建立的表结点的尾指针为NULL外，其余表结点的尾指针均指向在它之后建立的表结点。
+
+循环加递归
+*/
+Status StrAssign(SString &S,char chars[]){
+    int i=0;
+    while(chars[i]){
+        S[i+1]=chars[i];
+        i++;
+    }
+    S[0]=i;
+    return OK;
+}
+Status StrCompare(SString S,SString T){
+    if(S[0]==T[0]){
+        for(int i=1;i<=S[0];i++){
+            if(S[i]>T[i])   return 1;
+            if(S[i]<T[i])   return -1; 
+        }
+    }
+    return S[0]-T[0];
+}
+Status SubString(SString &sub,SString S,int pos,int len){
+    if(pos<1||pos>S[0]||len<0||len>S[0]-pos+1)  return ERROR;
+    sub[0]=len;
+    for(int i=pos,k=1;i<pos+len;i++,k++){
+        sub[k]=S[i];
+    }
+    return OK;
+}
+Status StrCopy(SString &T,SString S){
+    T[0]=S[0];
+    int i=1;
+    while(i<=T[0]){
+        T[i]=S[i];i++;
+    }
+    return OK;
+}
+Status ClearString(SString &S){
+    S[0]=0;
+    return OK;
+}
+Status sever(SString &str,SString &hstr){
+    int n=str[0];int i=0,k=0;
+    do{
+        i++;
+        if(str[i]=='(') ++k;
+        else if(str[i]==')')    --k;
+    }while(i<n&&(str[i]!=','||k!=0));
+    if(i<n){
+        SubString(hstr,str,1,i-1);SubString(str,str,i+1,n-i);
+    }else{StrCopy(hstr,str);ClearString(str);}
+    return OK;
+}
+Status CreateGHTList(GHTList &L,SString S){
+    SString emp;char chars[]="()";StrAssign(emp,chars);
+    if(StrCompare(S,emp)==0)    L=nullptr;
+    else{
+        L=(GHTList)malloc(sizeof(GHTLNode));
+        if(!L)  exit(OVERFLOW);
+        if(S[0]==1){
+            L->tag=ATOM;
+            L->atom=S[1];
+            printf("%c",L->atom);
+        }else{
+            L->tag=LIST;GHTList p=L,q=nullptr;
+            SString sub;
+            SubString(sub,S,2,S[0]-2);
+            
+            do{
+                
+                SString hsub;sever(sub,hsub);
+                CreateGHTList(p->ptr.hp,hsub);q=p;
+                if(sub[0]!=0){
+                    p=(GHTList)malloc(sizeof(GHTLNode));
+                    if(!p)  exit(OVERFLOW);
+                    p->tag=LIST;q->ptr.tp=p;
+                }
+            }while(sub[0]!=0);
+            q->ptr.tp=nullptr;
+        }
+    }
+    return OK;
+}
+void PrintGHTList(GHTList L) {
+    if (L==nullptr) {
+        printf("()");
+        return;
+    }
+    if (L->tag==ATOM) {
+        printf("%c", L->atom);
+    } else if (L->tag==LIST) {
+        printf("(");
+        if (L->ptr.hp) {
+            PrintGHTList(L->ptr.hp);  // 打印头部
+        }
+        GHTList p = L->ptr.tp;  // 开始处理尾部
+        while(p){
+            printf(", ");
+            PrintGHTList(p->ptr.hp);  // 打印头部
+            p = p->ptr.tp;  // 移动到下一个节点
+        }
+        printf(")");
+    }
+}
+
+
