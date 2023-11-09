ecbdf91969d6ad17d4ac555be72df903197a150e
QIT <2278949918@qq.com>
2023-11-09 12:13:38 +0800

    new

--- 练习/严书练习/树.cpp
+++ 练习/严书练习/树.cpp
@@ -2,3 +2,17 @@
 #include <stdlib.h>
 #include "Status.h"
 #include <limits.h>
+
+//TODO:二叉树的链式存储
+typedef int ElemType
+typedef struct BiTNode{
+    ElemType data;
+    struct BiTNode*lchild,*rchild;
+}BiTNode,*BiTree; 
+//TODO: 遍历的非递归算法
+//-----------------------------相关栈的定义及操作-------------------------
+
+void visit(BiTNode* T){
+    if(T!=nullptr)
+    printf("%d",T->data);
+}
